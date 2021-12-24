d5462a0f35dae3318c0dd7a704205f060d238d0d
Caster <daniel.dabrowski.177@gmail.com>
2021-12-24 23:47:40 +0100

    champagne

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,26 +1,25 @@
 // Menu & Navigation
 const menu = document.querySelector(".menu")
 const nav = document.querySelector(".nav")
-const sidebar = document.querySelector(".sidebar-wrapper")
-
+const sidebar = document.querySelector(".sidebar")
 const li = document.querySelectorAll("li")
+
+const about = document.querySelector(".about")
+const comments = document.querySelector(".comments")
+const contact = document.querySelector(".contact")
+const yOffset = -110
 
 menu.addEventListener("click", () => {
   nav.classList.remove("nav")
   nav.classList.add("sidebar")
-  sidebar.style = "display: ''"
+  nav.style = "display: ''"
+  menu.style = "display: none"
 })
+
+window.addEventListener("click", () => {})
 
 li.forEach((c) => {
   c.addEventListener("click", () => {
-    console.log(c.textContent)
-    sidebar.style = "display: none"
-    menu.style = "display: ''"
-
-    const about = document.querySelector(".about")
-    const comments = document.querySelector(".comments")
-    const contact = document.querySelector(".contact")
-    const yOffset = -110
     const aboutTop =
       about.getBoundingClientRect().top + window.pageYOffset + yOffset
     const commentsTop =
@@ -37,5 +36,46 @@ li.forEach((c) => {
     } else if (c.textContent == "Contact") {
       window.scrollTo(0, contactTop)
     }
+
+    if (window.screen.width >= 1024) {
+      nav.style = "display: ''"
+    } else {
+      nav.style = "display: none"
+      menu.style = "display: ''"
+    }
   })
+})
+
+const aboutTop =
+  about.getBoundingClientRect().top + window.pageYOffset + yOffset
+const commentsTop =
+  comments.getBoundingClientRect().top + window.pageYOffset + yOffset
+const contactTop =
+  contact.getBoundingClientRect().top + window.pageYOffset + yOffset
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    li.forEach((i) => {
+      i.classList.remove("active")
+    })
+    li[0].classList.add("active")
+  }
+  if (window.scrollY > aboutTop) {
+    li.forEach((i) => {
+      i.classList.remove("active")
+    })
+    li[1].classList.add("active")
+  }
+  if (window.scrollY > commentsTop) {
+    li.forEach((i) => {
+      i.classList.remove("active")
+    })
+    li[2].classList.add("active")
+  }
+  if (window.scrollY > contactTop) {
+    li.forEach((i) => {
+      i.classList.remove("active")
+    })
+    li[3].classList.add("active")
+  }
 })
